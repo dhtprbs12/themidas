8834ded816775aede0b9dba5a05dae07f40956a3
dhtprbs12 <dhtprbs12@gmail.com>
2020-08-17 19:48:57 -0500

    created most popular sections

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -8,6 +8,10 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MostPopular1 from './MostPopular1'
+import MostPopular2 from './MostPopular2'
+import MostPopular3 from './MostPopular3'
+import MostPopular4 from './MostPopular4'
 
 type FEEDBACK = {
   id: number,
@@ -17,35 +21,20 @@ type FEEDBACK = {
   feedback: string
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '50%',
-      margin: '0 auto'
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      flexBasis: '33.33%',
-      flexShrink: 0,
-    },
-    secondaryHeading: {
-      fontSize: theme.typography.pxToRem(15),
-      color: theme.palette.text.secondary,
-    },
-  }),
-);
 
 const Section: React.FC = () => {
 
-  const classes = useStyles();
   const { data } = useQuery(GET_FEEDBACKS)
 
   const { feedbacks = [] }: { feedbacks: Array<FEEDBACK> } = data || {}
 
   return (
     <div className="section">
-      <div className={classes.root}>
-
+      <div className='section-container'>
+        <MostPopular1 />
+        <MostPopular2 />
+        <MostPopular3 />
+        <MostPopular4 />
       </div>
     </div>
   );
